7cb558aa6fbad62b538154d1cdfb392daef27f9b
XingYunLi <contact@idrmfly.com>
2018-05-02 13:03:04 +0800

    修正回复@员带.号空格隔开错误

--- pkg/util/contentfmt.go
+++ pkg/util/contentfmt.go
@@ -38,7 +38,7 @@ var (
 	// #imgRegexp     = regexp.MustCompile(`(https?://[\w./:]+/[\w./]+\.(jpg|jpe|jpeg|gif|png))`)
 	imgRegexp     = regexp.MustCompile(`(https?://.+\.(jpg|jpe|jpeg|gif|png))`)
 	gistRegexp    = regexp.MustCompile(`(https?://gist\.github\.com/([a-zA-Z0-9-]+/)?[\d]+)`)
-	mentionRegexp = regexp.MustCompile(`\B@([a-zA-Z0-9\p{Han}]{1,32})\s?`)
+	mentionRegexp = regexp.MustCompile(`\B@([a-zA-Z0-9_\.\p{Han}]{1,32})\s?`)
 	urlRegexp     = regexp.MustCompile(`([^;"='>])(https?://[^\s<]+[^\s<.)])`)
 	nlineRegexp   = regexp.MustCompile(`\s{2,}`)
 	youku1Regexp  = regexp.MustCompile(`https?://player\.youku\.com/player\.php/sid/([a-zA-Z0-9=]+)/v\.swf`)
